0889dfb1a63796eeb1ba9c3dec26523fd033fcf4
Michael W <walkerrepair255@yahoo.com>
2022-02-04 16:08:20 -0500

    onClick with no args and onClick with args alsoaccessing event object.

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,20 @@
 const Home = () => {
+
+  const handleClick = (e) => {
+    console.log('hello, ninjas',e);
+  }
+
+  const handleClickAgain = (name,e) => {
+    console.log('hello ' + name, e);
+  }
+
   return (
     <div className="home">
       <h2>HomePage</h2>
+      <button onClick={handleClick} >Click Me</button>
+      <button onClick={(e) => 
+        handleClickAgain('Todd',e)
+      }>Click me again</button>
     </div>
   );
 }
