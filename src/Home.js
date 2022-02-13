621efd2b244668db7477b6a0c5af049aa62da686
Michael W <walkerrepair255@yahoo.com>
2022-02-12 19:56:40 -0500

    Separated my mapping function into a commponent Then used props to give my functions access to ve my func access to my blog list

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,16 @@
 import {useState} from 'react'
+import BlogList from './BlogList';
 
 
 const Home = () => {
-  //let name = 'mario';
-  const [name,setName] = useState('mario');
-  const [age, setAge] = useState(25);
-
-  const handleClick = () => {
-    setName('luigi');
-    setAge(30);
-  }
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1},
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2},
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'luigi', id: 3}
+  ]);
   return (
     <div className="home">
-      <h2>HomePage</h2>
-      <p>{name} is {age}</p>
-      <button onClick={handleClick}>Click Me</button>
+      <BlogList blogs={blogs} title="Wow!"/>
     </div>
   );
 }
